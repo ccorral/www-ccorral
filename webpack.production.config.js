d07b6bd46070a43377e6d4f9cceb74c47d87e5bd
Carlos Corral <carloscorral@Carloss-MacBook-Air.local>
2018-07-17 21:41:50 -0700

    New background and prod webpack build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -87,6 +87,9 @@ module.exports = {
       minimize: true,
       debug: false,
     }),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin({
       beautify: false,
       mangle: {
